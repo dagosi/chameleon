53cdf7f8378632281e6ee3551c19b3f67d10506f
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2018-12-11 19:25:21 -0500

    Change color on click

--- app/main_activity.rb
+++ app/main_activity.rb
@@ -1,5 +1,32 @@
 class MainActivity < Android::App::Activity
   def onCreate(savedInstanceState)
     super
+
+    layout = Android::Widget::LinearLayout.new(self)
+    layout.orientation = Android::Widget::LinearLayout::VERTICAL
+
+    text_view = Android::Widget::TextView.new(self)
+    text_view.text = "The Chameleon"
+    text_view.textSize = 25
+    layout.addView(text_view)
+
+    button = Android::Widget::Button.new(self)
+    button.text = "Colorize"
+    button.onClickListener = IncreaseButtonListener.new(text_view)
+    layout.addView(button)
+
+    self.contentView = layout
+  end
+end
+
+class IncreaseButtonListener
+  attr_reader :text_view
+
+  def initialize(text_view)
+    @text_view = text_view
+  end
+
+  def onClick(_view)
+    @text_view.textColor = Android::Graphics::Color.rgb(rand(255), rand(255), rand(255))
   end
 end
